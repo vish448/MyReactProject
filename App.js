227e92ff2032d08adcddf442764b77d8a775d9a7
vishang soni <vishangsoni@gmail.com>
2018-08-15 01:59:27 -0400

    Creating button component

--- App.js
+++ App.js
@@ -7,68 +7,39 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, TouchableHighlight, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; //Adding MaterialIcons
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 export default class App extends Component<Props> {
-  constructor(){
-    super();
-    console.log("constructor called");
-  };
-  state = {
-    greeting: "Hello World!"
-  }
-  updateGreeting = () => {
-    this.setState ({
-      greeting: "New Greeting"
-    })
-  }
+ onPress = () =>{
+   alert('Button Pressed')
+ };
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.box,{backgroundColor: 'red'}]}/>
-        <View style={[styles.box,{backgroundColor: 'green'}]}/>
-        <View style={[styles.box,{backgroundColor: 'blue'}]}/>
+        <TouchableHighlight
+          underlayColor='rgba(50,185,255,.8)'
+          onPress={this.onPress}
+          style={styles.button}>
+          <View style={styles.textContainer}>
+           <Icon name="home" size={30} color="#FFF" />
+            <Text style={styles.text}>Home</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-//// TODO: Todo test
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    //justifyContent: 'center',
-    //alignItems: 'center',
-    backgroundColor: 'pink',
-    //flexDirection:'row',//Available option column // Setting up flex direction. By default it's column. Setting up the Flex axis to Y
-    //justifyContent :'stretch', // Justify contents on Primary axis
-    //alignItems:'center', //Justify contents on Secondary Axis
+      flex: 1,
+      justifyContent:'center',
+      //alignItems: 'center'
+    },
+  button:{margin: 10, backgroundColor: 'rgba(50,183,255,1)', borderRadius: 30,padding: 12},
+  textContainer:{ justifyContent: 'center',alignItems:'center', flexDirection:'row'},
+  text: {marginLeft: 18, color: '#FFF', fontSize: 18},
 
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  box:{
-    //height: 100,
-    //width: 100,
-    //backgroundColor: 'green',
-    margin:10,
-    flex:1
-  },
+
 });
