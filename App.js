35143ab791002ef58851765530416b0d32f83d30
vishang soni <vishangsoni@gmail.com>
2018-08-15 22:27:39 -0400

    Using FlatList

--- App.js
+++ App.js
@@ -7,22 +7,39 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, TouchableHighlight, Text, View} from 'react-native';
+import {FlatList, StyleSheet, TouchableHighlight, Text, View} from 'react-native';
 
 
+const people = [
+  { name:'Vishang', age:33},
+  { name:'Nirali', age:28}
+]
 
 export default class App extends Component {
+
+  renderItems(){
+    return people.map(this.renderItem)
+  }
+  renderItem(person,index){
+    return(
+      <Text
+        key={index}
+        style={styles.text}>
+          {person.name} is {person.age}
+      </Text>
+    )
+  }
+
   render() {
-    const people = [
-      { name:'Vishang', age:33},
-      { name:'Nirali', age:28}
-    ]
+
     //array.map((item,index) => )
     return (
       <View style={styles.container}>
-        {
-          people.map((person,index) => <Text key={index} style={styles.text}>{person.name} is {person.age} </Text>)
-        }
+      <FlatList
+        data={people}
+        keyExtractor = {item => item.name}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+        />
       </View>
     );
   }
