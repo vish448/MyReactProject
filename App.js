7df061fb806b45b1fdf8fd4840a68319c9a1521a
vishang soni <vishangsoni@gmail.com>
2018-08-21 00:01:53 -0400

    Two way data binding with TextInput

--- App.js
+++ App.js
@@ -7,80 +7,23 @@
  */
 
 import React, {Component} from 'react';
-import {FlatList, StyleSheet, TouchableHighlight, Text, View} from 'react-native';
-
-
-/*const people = [
-  { name:'Sam', age:33},
-  { name:'Matt', age:28},
-  { name:'John', age:22},
-]*/
+import {FlatList, StyleSheet, TouchableHighlight, Text, View,TextInput} from 'react-native';
 
 export default class App extends Component {
-  state = {people: [], refreshing: false, page: 1}
-  async componentDidMount(){
-    this.fetchData()
-  }
-
-  fetchData = async() => {
-    this.setState({refreshing:true})
-    try {
-      const data = await fetch(`https://swapi.co/api/people?page=${this.state.page}`)
-      const json = await data.json()
-      this.setState({
-        people : json.results,
-        page : this.state.page + 1,
-        refreshing: false
-      })
-    }catch (err){
-      console.log('Error fetching date', err)
-    }
-  }
-
-/*  onRefresh = () => {
-    this.setState({ refreshing:true }, () => {
-      setTimeout(() => {
-        const newPeople = [
-          ...this.state.people,
-          { name: 'Linda', age:23},
-          { name: 'Nathan',age:27},
-          {name:'Jennifer',age:12},
-        ]
-        this.setState({people:newPeople,refreshing:false})
-      }, 2400)
-    })
-  }*/
-  renderItems(){
-    return people.map(this.renderItem)
-  }
-  renderItem = ({ item }) =>{
-    return(
-      <View style={styles.textContainer}>
-        <Text
-          style={styles.textHeading}>
-            {item.name}
-        </Text>
-        <Text
-          style={styles.text}>
-            Gender: {item.gender}
-        </Text>
-      </View>
-    )
-  }
-
+  state = { input : ''}
+   onChangeText = val =>{
+     this.setState({input:val})
+   }
   render() {
-
     //array.map((item,index) => )
     return (
       <View style={styles.container}>
         <Text>People:</Text>
-      <FlatList
-        onRefresh={this.fetchData}
-        refreshing={this.state.refreshing}
-        data={this.state.people}
-        keyExtractor = {item => item.name}
-        renderItem={this.renderItem}
-        />
+        <TextInput
+          onChangeText = {this.onChangeText}
+          style = {styles.input}
+          />
+        <Text>{this.state.input}</Text>
       </View>
     );
   }
@@ -104,5 +47,10 @@ const styles = StyleSheet.create({
     text:{
       fontSize: 14,
       color: '#ccc',
-    }
+    },
+    input:{
+      borderWidth: 1,
+      borderColor: '#ddd',
+      height: 32,
+    },
 });
