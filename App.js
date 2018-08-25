2d1a692f1ff588c2aaaa7d4f6b78742719926f26
vishang soni <vishangsoni@gmail.com>
2018-08-24 23:51:22 -0400

    Switch navigator demo

--- App.js
+++ App.js
@@ -1,50 +1,46 @@
 import React from 'react'
-import { View, Text, } from 'react-native'
-import { createDrawerNavigator, createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import { View, Text, ActivityIndicator} from 'react-native'
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation'
+
+//import { withAuthenticator } from 'aws-amplify-react-native'
 
 const Home = (props) => <View>
   <Text>Home</Text>
   <Text onPress={() => props.navigation.navigate('Page2')}>Go to page 2</Text>
-  <Text onPress={() => props.navigation.openDrawer()}>Open Drawer</Text>
 </View>
 
 const Page2 = (props) => <View>
   <Text>Page2</Text>
-  <Text onPress={() => props.navigation.goBack()}>Go back</Text>
- <Text onPress={() => props.navigation.openDrawer()}>Open Drawer</Text>
-
+  <Text onPress={() => props.navigation.goBack()}>Home</Text>
 </View>
 
-const Info = (props) =>
-  <Text>Info</Text>
-  //<Text onPress={() => props.navigation.goBack()}>Go back</Text>
+const Unauthorized = () => <View>
+  <Text>Unauthorized User</Text>
+</View>
 
-Home.navigationOptions = {
-  title: 'Home'
+class Initializing extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.navigation.navigate('Main')
+    }, 2000)
+  }
+  render() {
+    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator />
+      <Text>Initializing</Text>
+    </View>
+  }
 }
 
-Page2.navigationOptions = {
-  title: 'Page 2'
-}
-
-Info.navigationOptions = {
-  title: 'Info'
-}
-
-const stackNav = createStackNavigator({
+const Main = createStackNavigator({
   Home: { screen: Home },
   Page2: { screen: Page2 }
 })
 
-const tabNav = createBottomTabNavigator({
-  Home: { screen: stackNav },
-  Info: { screen: Info },
+const MainNav = createSwitchNavigator({
+  Initializing: { screen: Initializing },
+  Main: { screen: Main },
+  Unauthorized: { screen: Unauthorized }
 })
 
-const Drawer = createDrawerNavigator({
-  Home: { screen: Home },
-  Page2: { screen: Page2 },
-  Info: { screen: Info },
-})
-
-export default Drawer
+export default MainNav
